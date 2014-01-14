058ee0e175db87320969ae099d5130c3a3525398
Andrey Valyaev <dron.valyaev@gmail.com>
2014-01-14 22:40:11 +0400

    refactoring rotateOverX

--- main.cpp
+++ main.cpp
@@ -63,24 +63,6 @@ void pngsave(const string &filename, const vector<uint8_t> bitmap, int width, in
 
 typedef model::point<double, 3, cs::cartesian> point3d;
 
-point3d rotateOverZ(const point3d &point, double angle)
-{
-	class rotator : public strategy::transform::ublas_transformer<point3d, point3d, 3, 3> {
-	public :
-		rotator(const double &angle)
-			: ublas_transformer<point3d, point3d, 3, 3>(
-				 cos(angle), sin(angle), 0, 0,
-				-sin(angle), cos(angle), 0, 0,
-				 0,	     0,		 1, 0,
-				 0,          0,          0, 1)
-		{
-		}
-	};
-	point3d result;
-	rotator(angle).apply(point, result);
-	return result;
-}
-
 point3d rotateOverY(const point3d &point, double angle)
 {
 	return point3d(get<0>(point) * cos(angle) - get<2>(point) * sin(angle),
@@ -90,20 +72,9 @@ point3d rotateOverY(const point3d &point, double angle)
 
 point3d rotateOverX(const point3d &point, double angle)
 {
-	class rotator : public strategy::transform::ublas_transformer<point3d, point3d, 3, 3> {
-	public :
-		rotator(const double &angle)
-			: ublas_transformer<point3d, point3d, 3, 3>(
-				1, 0, 0, 0,
-				0, cos(angle), sin(angle), 0,
-				0, -sin(angle), cos(angle), 0,
-				0,          0,           0, 1)
-		{
-		}
-	};
-	point3d result;
-	rotator(angle).apply(point, result);
-	return result;
+	return point3d(get<0>(point),
+		       get<2>(point) * sin(angle) + get<1>(point) * cos(angle),
+		       get<2>(point) * cos(angle) - get<1>(point) * sin(angle));
 }
 
 int main(int /*argc*/, char **argv)
@@ -131,19 +102,6 @@ int main(int /*argc*/, char **argv)
 //		pngsave(lexical_cast<string>(n++) + ".png", f, 32, 48, palette);
 //		cout << "facing size: " << f.size() << endl;
 //	}
-
-//	const point3d one_vector(1, 0, 0);
-//	const point3d facing_vec = rotateOverZ(one_vector, -35.264 * math::d2r);
-//	const point3d facings_vec[8] = {
-//		rotateOverY(facing_vec, 45 * math::d2r),
-//		facing_vec,
-//		rotateOverY(facing_vec, -45 * math::d2r),
-//		rotateOverY(facing_vec, -90 * math::d2r),
-//		rotateOverY(facing_vec, -135 * math::d2r),
-//		rotateOverY(facing_vec, -180 * math::d2r),
-//		rotateOverY(facing_vec, -225 * math::d2r),
-//		rotateOverY(facing_vec, -270 * math::d2r)
-//	};
 
 	const int yangle[8] = { 135, 90, 45, 0, -45, -90, -135, -180 };
 
@@ -196,7 +154,7 @@ void CUSTOM_ASSERT_POINT_EQUAL(const point3d &a, const point3d &b)
 
 UP_TEST(rotationOverYShouldCorrect)
 {
-	point3d given(1, 0, 0);
+	const point3d given(1, 0, 0);
 	CUSTOM_ASSERT_POINT_EQUAL(rotateOverY(given, 45 * math::d2r), point3d(.707107, 0, .707107));
 	CUSTOM_ASSERT_POINT_EQUAL(rotateOverY(given, 90 * math::d2r), point3d(0, 0, 1));
 	CUSTOM_ASSERT_POINT_EQUAL(rotateOverY(given, 135 * math::d2r), point3d(-.707107, 0, .707107));
@@ -204,4 +162,10 @@ UP_TEST(rotationOverYShouldCorrect)
 	CUSTOM_ASSERT_POINT_EQUAL(rotateOverY(given, 225 * math::d2r), point3d(-.707107, 0, -.707107));
 	CUSTOM_ASSERT_POINT_EQUAL(rotateOverY(given, -90 * math::d2r), point3d(0, 0, -1));
 	CUSTOM_ASSERT_POINT_EQUAL(rotateOverY(given, -45 * math::d2r), point3d(.707107, 0, -.707107));
+}
+
+UP_TEST(rotationOverXShouldCorrect)
+{
+	const point3d given(0, 0, -1);
+	CUSTOM_ASSERT_POINT_EQUAL(rotateOverX(given, 35.264 * math::d2r), point3d(0, -.577345, -.816501));
 }
